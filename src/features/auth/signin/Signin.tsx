e960c5c34869f529cf14250d9f096e4b184b741a
Zuzanna Komar <s17301@pjwstk.edu.pl>
2024-04-20 12:12:32 +0200

    exp change IV

--- src/features/auth/signin/Signin.tsx
+++ src/features/auth/signin/Signin.tsx
@@ -35,7 +35,7 @@ export default function Signin() {
       credentials: "include",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({ email: data.email, password: data.password }),
     })
